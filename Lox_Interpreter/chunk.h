0ff40075aa69081a00b2d2f4be8ef3f8cab1980b
Vladi756 <kosticv@lsbu.ac.uk>
2022-10-07 22:55:12 +0100

    Initial Header Files

---
+++ Lox_Interpreter/chunk.h
@@ -0,0 +1,8 @@
+// Module to define code representation
+
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+#endif
---
+++ Lox_Interpreter/common.h
@@ -0,0 +1,11 @@
+// Types & Constants used throughout Interpreter go here
+
+#ifndef clox_common_h
+#define clox_common_h
+
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
