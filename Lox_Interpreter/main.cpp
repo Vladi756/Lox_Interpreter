2abc95ba33548d87cc036c3986f41fc8273b8c6a
Vladi756 <kosticv@lsbu.ac.uk>
2022-10-07 22:51:17 +0100

    Add project files.

---
+++ Lox_Interpreter/main.cpp
@@ -0,0 +1,7 @@
+#include "common.h"
+
+// argc is Argument count || How many arguments are being passed into the executable. 
+// argv is Argument Values || A pointer to an array of characters.
+int main(int argc, const char* argv[]) {
+	return 0;
+}
